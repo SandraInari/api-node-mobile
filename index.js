af634d9e9e6fbc0d5ae24cc69f7a4445a88aa3fd
Alessandra Bichara <khonsbichara@gmail.com>
2025-03-12 17:45:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const userController = require('../src/controllers/userController')
-
-const app = express();
-app.use(express.json())
-app.use('/api/users' , userController);
-
-app.listen(3000, () =>{
-    console.log('Servidor rodando na porta 3000');
-})
+console.log('Hello World!')
