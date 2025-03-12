a49273635b08f2636812657293923273f17d409e
Alessandra Bichara <khonsbichara@gmail.com>
2025-03-12 17:47:28 -0300

    Criação do projeto e configuração do banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize ({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+
+})
+
+sequelize.authenticate()
+            .then(() => {
+                console.log('Conexão feita com sucesso!')
+                return sequelize.sync()
+            })
+            .catch(err => {
+                console.error('Não foi possivel se conectar', err)
+
+            })
+
+module.exports = sequelize;
